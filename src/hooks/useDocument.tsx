ca6672a8a433e899709c2efdc879e13d01af9eaa
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2024-04-15 17:20:29 -0400

    feat: add useDocument hook (#4)
    https://yexttest.atlassian.net/browse/SUMO-5876?atlOrigin=eyJpIjoiNWU3YTllNjI3YzViNGVkNGExZWE4YjMyMGUyYjFjYzMiLCJwIjoiaiJ9

---
+++ src/hooks/useDocument.tsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, ReactNode } from "react";
+
+const DocumentContext = createContext<any | undefined>(undefined);
+
+type DocumentProviderProps<T> = {
+  value: T;
+  children: ReactNode;
+};
+
+const DocumentProvider = <T,>({ value, children }: DocumentProviderProps<T>) => {  
+  return (
+    <DocumentContext.Provider value={value}>
+      {children}
+    </DocumentContext.Provider>
+  );
+}
+
+const useDocument = <T,>(selector?: (state: T) => any): T => {
+  const context = useContext(DocumentContext);
+  if (!context) {
+    throw new Error(
+      "useLocationDocument must be used within a DocumentProvider"
+    );
+  }
+  
+  if (selector) {
+    return selector(context);
+  }
+  
+  return context as T;
+}
+
+export { DocumentProvider, useDocument };
