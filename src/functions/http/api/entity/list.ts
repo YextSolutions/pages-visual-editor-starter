1d4c1c080875be0bb6409dfca15d7277135e353f
Colton Demetriou <cdemetriou@yext.com>
2024-04-16 10:51:23 -0400

    User query params for list filter.

--- src/functions/http/api/entity/list.ts
+++ src/functions/http/api/entity/list.ts
@@ -3,30 +3,26 @@ import { PagesHttpRequest, PagesHttpResponse } from "@yext/pages/*";
 export default async function getEntities(
     request: PagesHttpRequest
   ): Promise<PagesHttpResponse> {
-    const { method, pathParams, body } = request;
+    const { method, queryParams, body } = request;
     if (method !== "GET") {
         return { body: "Method not allowed", headers: {}, statusCode: 405 };
     }
-  
-    const mgmtApiResp = await fetch(
-      `https://api.yextapis.com/v2/accounts/me/entities?api_key=${YEXT_PUBLIC_API_KEY}&v=20240401`
-    );
 
+    const vparam = 20240401;
+
+    let mgmtApiReq = `https://api.yextapis.com/v2/accounts/me/entities?api_key=${YEXT_PUBLIC_API_KEY}&v=${vparam}`;
+    const entityTypes = queryParams.entityTypes; 
+    if (entityTypes) {
+        mgmtApiReq += `&entityTypes=${entityTypes}`
+    }
+    console.log('here2');
+  
+    const mgmtApiResp = await fetch(mgmtApiReq);
     const resp = await mgmtApiResp.json();
 
-    if (mgmtApiResp.status !== 200) {
-        console.error("Error fetching entities:", resp);
-        return {
-            body: JSON.stringify(resp),
-            headers: {},
-            statusCode: mgmtApiResp.status,
-        };
-    }
-
-    const { response } = resp;
     return {
-        body: JSON.stringify(response),
+        body: JSON.stringify(resp),
         headers: {},
-        statusCode: 200,
+        statusCode: mgmtApiResp.status,
     };
   }
