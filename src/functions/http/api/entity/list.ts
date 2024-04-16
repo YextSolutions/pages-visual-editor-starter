bfc2b89ed98fc9a43185a900b084ce6ec1f3b3c8
Colton Demetriou <cdemetriou@yext.com>
2024-04-16 11:19:13 -0400

    Add try/catch

--- src/functions/http/api/entity/list.ts
+++ src/functions/http/api/entity/list.ts
@@ -1,5 +1,7 @@
 import { PagesHttpRequest, PagesHttpResponse } from "@yext/pages/*";
 
+const vparam = 20240401;
+
 export default async function getEntities(
     request: PagesHttpRequest
   ): Promise<PagesHttpResponse> {
@@ -8,20 +10,26 @@ export default async function getEntities(
         return { body: "Method not allowed", headers: {}, statusCode: 405 };
     }
 
-    const vparam = 20240401;
-
     let mgmtApiReq = `https://api.yextapis.com/v2/accounts/me/entities?api_key=${YEXT_PUBLIC_API_KEY}&v=${vparam}`;
     const entityTypes = queryParams.entityTypes; 
     if (entityTypes) {
         mgmtApiReq += `&entityTypes=${entityTypes}`
     }
-  
-    const mgmtApiResp = await fetch(mgmtApiReq);
-    const resp = await mgmtApiResp.json();
 
-    return {
-        body: JSON.stringify(resp),
-        headers: {},
-        statusCode: mgmtApiResp.status,
-    };
+    const mgmtApiResp = await fetch(mgmtApiReq);
+    try {
+        const resp = await mgmtApiResp.json();
+        return {
+            body: JSON.stringify(resp),
+            headers: {},
+            statusCode: mgmtApiResp.status,
+        };
+    } catch (error) {
+        console.log("Failed to fetch entities: " + error)
+        return {
+            body: JSON.stringify(mgmtApiResp),
+            headers: {},
+            statusCode: mgmtApiResp.status,
+        };
+    }
   }
