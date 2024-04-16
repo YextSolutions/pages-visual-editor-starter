2692d4d7889178878fb5a5b752bb6d8b6529b14c
Jacob Wartofsky <jwartofsky@yext.com>
2024-04-16 17:48:46 -0400

    use await instead of then

--- src/components/puck-overrides/Ajax.js
+++ src/components/puck-overrides/Ajax.js
@@ -3,19 +3,19 @@
  * @return {Promise<Entity[]>}
  */
 export async function fetchEntities() {
-  return fetch("api/entity/list").then((res) => {
+  const res = await fetch("api/entity/list").then((res) => {
     if (!res.ok) {
       throw new Error(res.statusText);
     }
     return res.json();
-  }).then(data => {
-    const entities = data["response"]["entities"];
-    return entities.map(entity => {
-      return {
-        name: entity["name"],
-        externalId: entity["meta"]["id"],
-        internalId: entity["meta"]["uid"],
-      }
-    });
+  })
+
+  const entities = res["response"]["entities"];
+  return entities.map(entity => {
+    return {
+      name: entity.name,
+      externalId: entity.meta.id,
+      internalId: entity.meta.uid,
+    }
   });
 }
