0b0c064d109d8e24ad7f4c763e0a938695a8819f
Jacob Wartofsky <jwartofsky@yext.com>
2024-04-16 17:05:41 -0400

    automatically select entity when there is only one

--- src/components/puck-overrides/EntityPicker.tsx
+++ src/components/puck-overrides/EntityPicker.tsx
@@ -16,23 +16,50 @@ type Entity = {
   internalId: number,
 }
 
+const urlFromEntity = (entity: Entity) => {
+  return `edit?entityId=${entity.internalId}`;
+}
+
 export function EntityPicker() {
+  const [entity, setEntity] = useState<Entity>();
   const [entities, setEntities] = useState<Entity[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const entityId = urlParams.get("entityId");
 
   useEffect(() => {
     fetchEntities().then((fetched) => {
+      setLoading(false);
       setEntities(fetched);
+      if (fetched.length == 1) {
+        setEntity(fetched[0]);
+        const targetUrl = urlFromEntity(fetched[0]);
+        if (!window.location.href.includes(targetUrl)) {
+          window.location.href = targetUrl;
+        }
+      } else {
+        fetched.forEach(e => {
+          if (e.internalId == entityId) {
+            setEntity(e);
+          }
+        })
+      }
     });
   }, []);
 
-  const list = entities.map((entity: Entity) => <MenuItem as='a' key={entity.internalId} href={`edit?entityId=${entity.internalId}`}>{entity.name}</MenuItem>);
+  const list = entities.map((e: Entity) => <MenuItem as={Button} key={e.internalId} onClick={() => {
+    setEntity(e);
+    window.location.href = urlFromEntity(e);
+  }}>{e.name}</MenuItem>);
 
   return (
       <ChakraProvider>
         <div className='entity-picker'>
           <Menu>
-            <MenuButton as={Button} className="dropdown-button">
-              Entity <ChevronDownIcon/>
+            <MenuButton as={Button} className="dropdown-button" variant={entity ? "solid" : "ghost"} isActive={!!entity} isLoading={loading}>
+              {entity ? entity.name : "Entity"}
+              <ChevronDownIcon/>
             </MenuButton>
             <MenuList>
               {list}
