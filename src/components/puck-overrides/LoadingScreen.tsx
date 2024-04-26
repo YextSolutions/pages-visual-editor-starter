03e9b0de2acd1cb2e3e9791848624248713f95f5
Jacob <115099870+jwartofsky-yext@users.noreply.github.com>
2024-04-26 12:40:23 -0400

    feat: add loading screen (#23)
    Adds a loading screen when loading the visual editor.
    
    Consolidates pulling data to one place in the code.
    
    
    https://github.com/YextSolutions/pages-visual-editor-starter/assets/115099870/1d28d646-dd3c-434f-8130-62446e8cbfd0

---
+++ src/components/puck-overrides/LoadingScreen.tsx
@@ -0,0 +1,25 @@
+import { Center, Flex, Progress, VStack } from "@chakra-ui/react";
+
+export interface LoadingScreenProps {
+  message: string;
+  progress: number;
+}
+
+export function LoadingScreen({ message, progress }: LoadingScreenProps) {
+  return (
+    <div className="loading-screen">
+      <Flex className={"wrapper"}>
+        <Center className={"wrapper"}>
+          <VStack className={"wrapper"}>
+            <Progress
+              className="progress"
+              value={progress}
+              hasStripe
+            ></Progress>
+            <div>{message}</div>
+          </VStack>
+        </Center>
+      </Flex>
+    </div>
+  );
+}
