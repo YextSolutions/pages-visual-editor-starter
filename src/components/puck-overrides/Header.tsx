13de549971ee4ef085edff5e54dfd0eae63bab95
Alexis Sanehisa <asanehisa@yext.com>
2024-05-14 11:44:47 -0400

    add right / left toggle
    Change-Id: I4eea9a8099c5c6a6c290cf5e1e17a920f931db89

--- src/components/puck-overrides/Header.tsx
+++ src/components/puck-overrides/Header.tsx
@@ -4,8 +4,9 @@ import "./puck.css";
 import { TemplateDefinition, TemplatePicker } from "./TemplatePicker";
 import { useDocument } from "../../hooks/useDocument";
 import { usePuck } from "@measured/puck";
-import { RotateCcw, RotateCw } from "lucide-react"
+import { PanelLeft, PanelRight, RotateCcw, RotateCw } from "lucide-react"
 import * as buttons from "../ui/Button"
+import { useCallback } from "react";
 
 
 const handleClick = (slug: string) => {
@@ -22,10 +23,10 @@ export const customHeaderActions = (children: any) => {
     <>
       {children}
       <buttons.Button variant="ghost" size="icon" disabled={!hasPast} onClick={back}>
-        <RotateCcw className="h-4 w-4" />
+        <RotateCcw className="sm-icon" />
       </buttons.Button>
       <buttons.Button variant="ghost" size="icon" disabled={!hasFuture} onClick={forward}>
-        <RotateCw className="h-4 w-4" />
+        <RotateCw className="sm-icon" />
       </buttons.Button>
       <Button onClick={() => handleClick(entityDocument.slug)}>
         Live Preview
@@ -51,7 +52,9 @@ export const customHeader = ({
 }: customHeaderProps) => {
   return (
     <header className="puck-header">
-      <div className="header-left" />
+      <div className="header-left">
+        <ToggleUIButtons/>
+      </div>
       <div className="header-center">
         <TemplatePicker selectedTemplate={template} templates={templates} />
         <EntityPicker selectedEntity={entity} entities={entities} />
@@ -60,3 +63,45 @@ export const customHeader = ({
     </header>
   );
 };
+
+const ToggleUIButtons = () => {  
+  const {
+    dispatch,
+    appState: {
+      ui: {
+        leftSideBarVisible, 
+        rightSideBarVisible
+      }
+    }
+  } = usePuck();
+
+  const toggleSidebars = useCallback(
+    (sidebar: "left" | "right") => {
+      const widerViewport = window.matchMedia("(min-width: 638px)").matches;
+      const sideBarVisible =
+        sidebar === "left" ? leftSideBarVisible : rightSideBarVisible;
+      const oppositeSideBar =
+        sidebar === "left" ? "rightSideBarVisible" : "leftSideBarVisible";
+
+      dispatch({
+        type: "setUi",
+        ui: {
+          [`${sidebar}SideBarVisible`]: !sideBarVisible,
+          ...(!widerViewport ? { [oppositeSideBar]: false } : {}),
+        },
+      });
+    },
+    [dispatch, leftSideBarVisible, rightSideBarVisible]
+  );
+
+  return (
+    <>
+      <buttons.Button variant="ghost" size="icon" onClick={() => {toggleSidebars("left")}}>
+        <PanelLeft className="sm-icon" />
+      </buttons.Button>
+      <buttons.Button variant="ghost" size="icon" onClick={() => {toggleSidebars("right")}}>
+        <PanelRight className="sm-icon" />
+      </buttons.Button>
+    </>
+  )
+}
