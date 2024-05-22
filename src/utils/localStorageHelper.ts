42427358ca71876611200f42584a63e367700655
Jacob Wartofsky <jwartofsky@yext.com>
2024-05-22 14:00:32 -0400

    do not use entityId on local storage key unless role is 'individual'

--- src/utils/localStorageHelper.ts
+++ src/utils/localStorageHelper.ts
@@ -1,5 +1,7 @@
+import {Role} from "../templates/edit";
+
 export function getLocalStorageKey(role: string, templateId: string, entityId?: string) {
-  if (entityId) {
+  if (role === Role.INDIVIDUAL && entityId) {
     return role + "_" + templateId + "_" + entityId;
   }
   return role + "_" + templateId;
