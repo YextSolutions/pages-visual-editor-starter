470179524e7bef4d90f3787f40b34c78d7ebfed7
apav-dev <apavlick@yext.com>
2024-06-03 13:13:47 -0400

    comment out stream and product stuff

--- src/puck/puck.config.tsx
+++ src/puck/puck.config.tsx
@@ -1,59 +1,59 @@
 import type { Config } from "@measured/puck";
 import { Toggle, ToggleProps } from "../components/Toggle";
-import { Hero, HeroProps } from '../components/Hero';
+import { Hero, HeroProps } from "../components/Hero";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { LocationCore } from "../components/LocationCore";
 import { LocationHero } from "../components/LocationHero";
 
-type LocationProps = {
-  Toggle: ToggleProps;
-  Hero: HeroProps;
-  LocationHero: any;
-  LocationCore: any;
-};
+// type LocationProps = {
+//   Toggle: ToggleProps;
+//   Hero: HeroProps;
+//   LocationHero: any;
+//   LocationCore: any;
+// };
 
-type ProductProps = {
-  Toggle: ToggleProps;
-  Hero: HeroProps;
-};
+// type ProductProps = {
+//   Toggle: ToggleProps;
+//   Hero: HeroProps;
+// };
 
 type FinancialProfessionalProps = {
   Toggle: ToggleProps;
 };
 
 // All the available components for locations
-export const locationConfig: Config<LocationProps> = {
-  components: {
-    Toggle,
-    Hero,
-    LocationHero,
-    LocationCore,
-  },
-  root: {
-    render: ({ children }) => {
-      return (
-      <>
-        <Header/>
-          {children}
-        <Footer/>
-      </>
-      );
-    },
-    fields: {},
-  },
-};
+// export const locationConfig: Config<LocationProps> = {
+//   components: {
+//     Toggle,
+//     Hero,
+//     LocationHero,
+//     LocationCore,
+//   },
+//   root: {
+//     render: ({ children }) => {
+//       return (
+//       <>
+//         <Header/>
+//           {children}
+//         <Footer/>
+//       </>
+//       );
+//     },
+//     fields: {},
+//   },
+// };
 
 // All the available components for products
-export const productConfig: Config<ProductProps> = {
-  components: {
-    Toggle,
-    Hero,
-  },
-  root: {
-    fields: {},
-  },
-};
+// export const productConfig: Config<ProductProps> = {
+//   components: {
+//     Toggle,
+//     Hero,
+//   },
+//   root: {
+//     fields: {},
+//   },
+// };
 
 // All the available components for financial professionals
 export const financialProfessionalConfig: Config<FinancialProfessionalProps> = {
@@ -66,7 +66,7 @@ export const financialProfessionalConfig: Config<FinancialProfessionalProps> = {
 };
 
 export const puckConfigs = new Map<string, Config>([
-  ["location", locationConfig],
-  ["product", productConfig],
+  // ["location", locationConfig],
+  // ["product", productConfig],
   ["financialProfessional", financialProfessionalConfig],
 ]);
