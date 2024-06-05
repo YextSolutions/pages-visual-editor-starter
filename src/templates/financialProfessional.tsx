83765b6f4a8c979fbcac22134672277bade71c1d
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2024-06-05 12:15:14 -0400

    Initial commit

---
+++ src/templates/financialProfessional.tsx
@@ -0,0 +1,94 @@
+import "../index.css";
+import {
+  Template,
+  GetPath,
+  TemplateConfig,
+  TemplateProps,
+  TemplateRenderProps,
+  GetHeadConfig,
+  HeadConfig,
+} from "@yext/pages";
+import { Config, Render } from "@measured/puck";
+import { financialProfessionalConfig } from "../puck/puck.config";
+import { DocumentProvider } from "../hooks/useDocument";
+import { getTemplatePuckData } from "../utils/puckDataHelper";
+
+export const config: TemplateConfig = {
+  name: "financialProfessional",
+  stream: {
+    $id: "financialProfessional-stream",
+    filter: {
+      entityTypes: ["financialProfessional"],
+    },
+    fields: [
+      "id", 
+      "name", 
+      "slug",
+      "c_visualConfigurations",
+      "c_pages_layouts.c_visualConfiguration"
+    ],
+    localization: {
+      locales: ["en"],
+    },
+  },
+};
+
+// Right now financial professional entity data isn't used
+export const transformProps = async (data) => {
+  const { document } = data;
+  const entityConfigurations = document.c_visualConfigurations ?? [];
+  const entityLayoutConfigurations = document.c_pages_layouts ?? [];
+  const siteLayoutConfigurations = document._site?.c_visualLayouts;
+  try {
+    const templateData = getTemplatePuckData(entityConfigurations, entityLayoutConfigurations, siteLayoutConfigurations, config.name);
+    const visualTemplate = JSON.parse(templateData);
+    return {
+      ...data,
+      document: {
+        ...document,
+        visualTemplate,
+      },
+    };
+  } catch (error) {
+    console.error("Failed to parse visualTemplate: " + error);
+    return data;
+  }
+};
+
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  document,
+}): HeadConfig => {
+  return {
+    title: document.name,
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+    tags: [
+      {
+        type: "link",
+        attributes: {
+          rel: "icon",
+          type: "image/x-icon",
+        },
+      },
+    ],
+  };
+};
+
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+  return `financialProfessional/${document.slug || document.name || document.id}`;
+};
+
+const FinancialProfessional: Template<TemplateRenderProps> = ({ document }) => {
+  const { visualTemplate, name } = document;
+  return (
+    <DocumentProvider value={document}>
+      <div>Welcome to the professional page for {name}!</div>
+      <Render
+        config={financialProfessionalConfig as Config}
+        data={visualTemplate}
+      />
+    </DocumentProvider>
+  );
+};
+
+export default FinancialProfessional;
