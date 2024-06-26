70fc00af50e5b32e58b8a9d34992b8f2470b9428
Brian Baugher <bbaugher@yext.com>
2024-06-26 16:13:10 -0400

    style

--- src/components/HoursCard.tsx
+++ src/components/HoursCard.tsx
@@ -11,14 +11,25 @@ const hoursCardFields: Fields<HoursCardProps> = {};
 const HoursCard = ({}: HoursCardProps) => {
   const hours = useDocument<LocationStream>(document => document.hours);
 
+  const css = `
+      .HoursTable-row: {
+        display: inline-flex;
+        justify-content: space-between;
+        gap: 30px;
+      }
+  `
+
   return (
       <Section
         className='flex flex-col justify-center components'
         padding='small'
       >
-        <Heading>Hours</Heading>
-        <div className='flex flex-col justify-center gap-y-3'>
-          <HoursTable hours={hours}/>
+        <style>{css}</style>
+        <div>
+          <Heading>Hours</Heading>
+          <div className='flex flex-col justify-center gap-y-3'>
+            <HoursTable hours={hours}/>
+          </div>
         </div>
       </Section>
   );
