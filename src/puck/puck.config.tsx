42e52791c3353fa233f4948cdf8d393dad7f1b7a
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2024-06-26 13:15:00 -0400

    feat: add columns component  (#66)
    <img width="1840" alt="Screenshot 2024-06-25 at 4 08 21 PM"
    src="https://github.com/YextSolutions/pages-visual-editor-starter/assets/106991746/4ff7cd24-532f-4d3e-ba6c-c0ac997627bf">
    
    Code + logic is from
    [Puck](https://github.com/measuredco/puck/blob/main/apps/demo/config/blocks/Columns/index.tsx)

--- src/puck/puck.config.tsx
+++ src/puck/puck.config.tsx
@@ -1,16 +1,19 @@
 import type { Config } from "@measured/puck";
 import { HeroComponent as Hero, HeroProps } from "../components/Hero";
+import { ColumnsComponent as Columns, ColumnsProps } from "../components/Columns";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
 type LocationProps = {
   Hero: HeroProps;
+  Columns: ColumnsProps;
 };
 
 // All the available components for locations
 export const locationConfig: Config<LocationProps> = {
   components: {
     Hero,
+    Columns
   },
   root: {
     render: ({ children }) => {
