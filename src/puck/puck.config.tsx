a1dee9ef6a44ab94ceb11101065c0c359b7a47ab
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2024-06-27 13:17:34 -0400

    feat: add card component (#67)
    Here is my [test
    site](https://www.yext.com/s/4174974/yextsites/147830/editor#templateId=location&layoutId=locationVisualConfiguration&view=EDIT)
    with my changes.
    
    [link to video
    ](https://drive.google.com/file/d/1ERur50hOq8gkrf6NyljOU7ZH59SqAPqa/view?usp=sharing)

--- src/puck/puck.config.tsx
+++ src/puck/puck.config.tsx
@@ -3,17 +3,20 @@ import { HeroComponent as Hero, HeroProps } from "../components/Hero";
 import { ColumnsComponent as Columns, ColumnsProps } from "../components/Columns";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { CardComponent as Card, CardProps } from "../components/Card";
 
 type LocationProps = {
   Hero: HeroProps;
   Columns: ColumnsProps;
+  Card: CardProps;
 };
 
 // All the available components for locations
 export const locationConfig: Config<LocationProps> = {
   components: {
     Hero,
-    Columns
+    Columns,
+    Card,
   },
   root: {
     render: ({ children }) => {
