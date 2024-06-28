7814c5014cfc30ee634f1fc956d216ff60ddd551
Brian Baugher <bbaugher@yext.com>
2024-06-28 13:35:43 -0400

    add props

--- src/components/HoursCard.tsx
+++ src/components/HoursCard.tsx
@@ -1,15 +1,82 @@
 import {ComponentConfig, Fields} from "@measured/puck";
 import {useDocument} from "../hooks/useDocument";
 import {LocationStream, Hours} from "../types/autogen";
-import {HoursTable, HoursType} from "@yext/pages-components";
+import {HoursTable, HoursTableProps, HoursType} from "@yext/pages-components";
 import {Section} from "./atoms/section";
-import {Heading} from "./atoms/heading";
+import {Heading, HeadingProps} from "./atoms/heading";
 import "@yext/pages-components/style.css";
 
-export type HoursCardProps = {};
-const hoursCardFields: Fields<HoursCardProps> = {};
+export type HoursCardProps = {
+  heading: {
+    text: string;
+    size: HeadingProps["size"];
+    color: HeadingProps["color"];
+  };
+  startOfWeek: "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday" | "today";
+  collapseDays: boolean;
+  showAdditionalHoursText: boolean;
+};
+const hoursCardFields: Fields<HoursCardProps> = {
+  heading: {
+    type: "object",
+    label: "Heading",
+    objectFields: {
+      text: {
+        label: "Text",
+        type: "text"
+      },
+      size: {
+        label: "Size",
+        type: "radio",
+        options: [
+          { label: "Section", value: "section" },
+          { label: "Subheading", value: "subheading" },
+        ],
+      },
+      color: {
+        label: "Color",
+        type: "radio",
+        options: [
+          { label: "Default", value: "default" },
+          { label: "Primary", value: "primary" },
+          { label: "Secondary", value: "secondary" },
+        ],
+      },
+    },
+  },
+  startOfWeek: {
+    label: "Start of the week",
+    type: "radio",
+    options: [
+      {label: "Monday", value: "monday"},
+      {label: "Tuesday", value: "tuesday"},
+      {label: "Wednesday", value: "wednesday"},
+      {label: "Thursday", value: "thursday"},
+      {label: "Friday", value: "friday"},
+      {label: "Saturday", value: "saturday"},
+      {label: "Sunday", value: "sunday"},
+      {label: "Today", value: "today"},
+    ],
+  },
+  collapseDays: {
+    label: "Collapse days",
+    type: "radio",
+    options: [
+      {label: "Yes", value: true},
+      {label: "No", value: false},
+    ],
+  },
+  showAdditionalHoursText: {
+    label: "Show additional hours text",
+    type: "radio",
+    options: [
+      {label: "Yes", value: true},
+      {label: "No", value: false},
+    ],
+  },
+};
 
-const HoursCard = ({}: HoursCardProps) => {
+const HoursCard = ({heading, startOfWeek, collapseDays, showAdditionalHoursText}: HoursCardProps) => {
   const hours:Hours = useDocument<LocationStream>(document => document.hours);
   const additionalHoursText:string = useDocument<LocationStream>(document => document.additionalHoursText);
 
@@ -26,9 +93,20 @@ const HoursCard = ({}: HoursCardProps) => {
       >
         <style>{css}</style>
         <div>
-          <Heading level={2} size={'subheading'} className={'mb-4'}>Hours</Heading>
-          <HoursTable hours={hours as HoursType} startOfWeek={'monday'}/>
-          {additionalHoursText && (
+          <Heading
+              level={2}
+              size={heading.size}
+              className={'mb-4'}
+              color={heading.color}
+          >
+            {heading.text}
+          </Heading>
+          <HoursTable
+              hours={hours as HoursType}
+              startOfWeek={startOfWeek}
+              collapseDays={collapseDays}
+          />
+          {(additionalHoursText & showAdditionalHoursText) && (
               <div className="mt-4">{additionalHoursText}</div>
           )}
         </div>
@@ -38,9 +116,23 @@ const HoursCard = ({}: HoursCardProps) => {
 
 export const HoursCardComponent: ComponentConfig<HoursCardProps> = {
   fields: hoursCardFields,
-  defaultProps: {},
+  defaultProps: {
+    heading: {
+      text: "Hours",
+      size: "subheading",
+      color: "default",
+    },
+    startOfWeek: "today",
+    collapseDays: false,
+    showAdditionalHoursText: true,
+  },
   label: 'Hours Card',
-  render: ({}) => (
-    <HoursCard/>
+  render: ({heading, startOfWeek, collapseDays, showAdditionalHoursText}) => (
+    <HoursCard
+      heading={heading}
+      startOfWeek={startOfWeek}
+      collapseDays={collapseDays}
+      showAdditionalHoursText={showAdditionalHoursText}
+    />
   ),
 };
