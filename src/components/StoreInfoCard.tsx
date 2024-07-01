36f1e44a1a831b9f386f6037f9f3b1b9948dae94
Colton Demetriou <cdemetriou@yext.com>
2024-07-01 14:56:32 -0400

    Update styling to mock
    Change-Id: I62dd8f8b740aa736238974821f0575f74e541781

--- src/components/StoreInfoCard.tsx
+++ src/components/StoreInfoCard.tsx
@@ -1,9 +1,9 @@
-import {ComponentConfig, Fields} from "@measured/puck";
-import {useDocument} from "../hooks/useDocument";
-import {LocationStream} from "../types/autogen";
-import { Address, getDirections} from "@yext/pages-components";
-import {Section} from "./atoms/section";
-import {Heading, HeadingProps} from "./atoms/heading";
+import { ComponentConfig, Fields } from "@measured/puck";
+import { useDocument } from "../hooks/useDocument";
+import { LocationStream } from "../types/autogen";
+import { Address, getDirections } from "@yext/pages-components";
+import { Section } from "./atoms/section";
+import { Heading, HeadingProps } from "./atoms/heading";
 import { Link } from "@yext/pages-components";
 import { IconContext } from "react-icons";
 import { MdOutlineEmail } from "react-icons/md";
@@ -13,115 +13,125 @@ import { HiOutlinePhone } from "react-icons/hi";
 import "@yext/pages-components/style.css";
 
 export type StoreInfoCardProps = {
-  heading: {
-    text: string;
-    size: HeadingProps["size"];
-    color: HeadingProps["color"];
-  };
+    heading: {
+        text: string;
+        size: HeadingProps["size"];
+        color: HeadingProps["color"];
+    };
 };
 const storeInfoCardFields: Fields<StoreInfoCardProps> = {
-  heading: {
-    type: "object",
-    label: "Heading",
-    objectFields: {
-      text: {
-        label: "Text",
-        type: "text"
-      },
-      size: {
-        label: "Size",
-        type: "radio",
-        options: [
-          { label: "Section", value: "section" },
-          { label: "Subheading", value: "subheading" },
-        ],
-      },
-      color: {
-        label: "Color",
-        type: "radio",
-        options: [
-          { label: "Default", value: "default" },
-          { label: "Primary", value: "primary" },
-          { label: "Secondary", value: "secondary" },
-        ],
-      },
+    heading: {
+        type: "object",
+        label: "Heading",
+        objectFields: {
+            text: {
+                label: "Text",
+                type: "text"
+            },
+            size: {
+                label: "Size",
+                type: "radio",
+                options: [
+                    { label: "Section", value: "section" },
+                    { label: "Subheading", value: "subheading" },
+                ],
+            },
+            color: {
+                label: "Color",
+                type: "radio",
+                options: [
+                    { label: "Default", value: "default" },
+                    { label: "Primary", value: "primary" },
+                    { label: "Secondary", value: "secondary" },
+                ],
+            },
+        },
     },
-  },
 };
 
-const StoreInfoCard = ({heading}: StoreInfoCardProps) => {
-  const address = useDocument<LocationStream>((document) => document.address);
-  const phoneNumber = formatPhoneNumber(useDocument<LocationStream>((document) => document.mainPhone));
-  const emails = useDocument<LocationStream>((document) => document.emails);
-  const coordinates = getDirections(address);
- 
-  return (
-      <Section
-        className='flex flex-col justify-center components items-center'
-        padding='small'
-      >
-        <div>
-            <Heading
-                level={2}
-                size={heading.size}
-                className={'mb-4'}
-                color={heading.color}
-            >
-                {heading.text}
-            </Heading>
-            <Address
-                address={address}
-                lines={[['line1'],['line2', 'city', 'region', 'postalCode']]}
-            />
-            <Link 
-                cta={{link: coordinates, label: "Get Directions", linkType: "URL"}}
-                className="font-bold hover:underline md:px-4 color:red"
-            />
-            <IconContext.Provider
-            value={{ color: 'red'}}
-            >
-            <div style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "stretch"}}>
-                <HiOutlinePhone />
-                {' ' + phoneNumber}
+const StoreInfoCard = ({ heading }: StoreInfoCardProps) => {
+    const address = useDocument<LocationStream>((document) => document.address);
+    const phoneNumber = formatPhoneNumber(useDocument<LocationStream>((document) => document.mainPhone));
+    const emails = useDocument<LocationStream>((document) => document.emails);
+    const coordinates = getDirections(address);
+
+    return (
+        <Section
+            className='flex flex-col justify-center components items-center'
+            padding='small'
+        >
+            <div>
+                <Heading
+                    level={2}
+                    size={heading.size}
+                    className={'mb-4'}
+                    color={heading.color}
+                >
+                    {heading.text}
+                </Heading>
+                <Address
+                    address={address}
+                    lines={[['line1'], ['line2', 'city', 'region', 'postalCode']]}
+                />
+                <div style={{
+                    paddingTop: "5px"
+                }}>
+                    <Link
+                        cta={{ link: coordinates, label: "Get Directions", linkType: "URL" }}
+                        className="font-bold text-primary hover:underline md:px-4"
+                    />
+                </div>
+                <IconContext.Provider
+                    value={{ color: 'red' }}
+                >
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "5px",
+                        alignItems: "center",
+                        paddingTop: "5px"
+                    }}>
+                        <HiOutlinePhone />
+                        {phoneNumber}
+                    </div>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "5px",
+                        alignItems: "center",
+                        paddingTop: "5px"
+                    }}>
+                        <MdOutlineEmail />
+                        {emails}
+                    </div>
+                </IconContext.Provider>
             </div>
-            <div style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "stretch"}}>
-                <MdOutlineEmail/>
-                {' ' + emails}
-            </div>
-            </IconContext.Provider>
-        </div>
-      </Section>
-  );
+        </Section>
+    );
 };
 
 export const StoreInfoCardComponent: ComponentConfig<StoreInfoCardProps> = {
-  fields: storeInfoCardFields,
-  defaultProps: {
-    heading: {
-      text: "Information",
-      size: "subheading",
-      color: "default",
+    fields: storeInfoCardFields,
+    defaultProps: {
+        heading: {
+            text: "Information",
+            size: "subheading",
+            color: "default",
+        },
     },
-  },
-  label: 'Store Info Card',
-  render: ({heading}) => (
-    <StoreInfoCard
-      heading={heading}
-    />
-  ),
+    label: 'Store Info Card',
+    render: ({ heading }) => (
+        <StoreInfoCard
+            heading={heading}
+        />
+    ),
 };
 
-function formatPhoneNumber(phoneNumberString:string) {
+function formatPhoneNumber(phoneNumberString: string) {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
     var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
     if (match) {
-      return ['(', match[2], ') ', match[3], '-', match[4]].join('');
+        return ['(', match[2], ') ', match[3], '-', match[4]].join('');
     }
     return null;
-  }
+}
