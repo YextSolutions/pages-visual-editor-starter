248bbb0a9dfb29bd0308b0e048f6e09d50585c1f
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2024-07-01 15:13:56 -0400

    feat: add banner component (#73)
    https://github.com/YextSolutions/pages-visual-editor-starter/assets/106991746/efbd2349-2e9f-45c0-b0b6-423a18671480
    
    can test on this
    [branch](https://www.yext.com/s/4174974/yextsites/147830/editor#templateId=location&layoutId=locationVisualConfiguration&view=EDIT)

--- src/puck/puck.config.tsx
+++ src/puck/puck.config.tsx
@@ -4,12 +4,14 @@ import { ColumnsComponent as Columns, ColumnsProps } from "../components/Columns
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { CardComponent as Card, CardProps } from "../components/Card";
+import { BannerComponent as Banner, BannerProps } from "../components/Banner";
 import {HoursCardComponent as HoursCard, HoursCardProps} from "../components/HoursCard";
 
 type LocationProps = {
   Hero: HeroProps;
   Columns: ColumnsProps;
   Card: CardProps;
+  Banner: BannerProps;
   HoursCard: HoursCardProps;
 };
 
@@ -19,6 +21,7 @@ export const locationConfig: Config<LocationProps> = {
     Hero,
     Columns,
     Card,
+    Banner,
     HoursCard,
   },
   root: {
