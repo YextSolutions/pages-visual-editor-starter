a11eb22c5a01cfeb7fa1550c3af4a12b45d3036e
apav-dev <apavlick@yext.com>
2024-07-01 14:58:12 -0400

    promo image formatting

--- src/components/DeliveryPromo.tsx
+++ src/components/DeliveryPromo.tsx
@@ -1,33 +1,34 @@
-import {ComponentConfig, Fields} from '@measured/puck';
-import {Heading, HeadingProps} from './atoms/heading';
-import {Body, BodyProps} from './atoms/body';
-import {ButtonProps} from './atoms/button';
-import {C_deliveryPromo, LocationStream} from "../types/autogen";
-import {useDocument} from "../hooks/useDocument";
-import {Image} from "@yext/pages-components";
-import {CTA} from "./atoms/cta";
-import {Section} from "./atoms/section";
-import {cn} from "../utils/cn";
+import { ComponentConfig, Fields } from "@measured/puck";
+import { Heading, HeadingProps } from "./atoms/heading";
+import { Body, BodyProps } from "./atoms/body";
+import { ButtonProps } from "./atoms/button";
+import { C_deliveryPromo, LocationStream } from "../types/autogen";
+import { useDocument } from "../hooks/useDocument";
+import { Image } from "@yext/pages-components";
+import { CTA } from "./atoms/cta";
+import { Section } from "./atoms/section";
+import { cn } from "../utils/cn";
 import "./index.css";
+import useScreenSizes from "../hooks/useDeviceSizes";
 
 export type DeliveryPromoProps = {
-  imageMode: 'left' | 'right';
+  imageMode: "left" | "right";
   promoTitle: {
-    size: HeadingProps['size'];
-    color: HeadingProps['color'];
+    size: HeadingProps["size"];
+    color: HeadingProps["color"];
   };
   promoDescription: {
-    size: BodyProps['size'];
-    weight: BodyProps['weight'];
+    size: BodyProps["size"];
+    weight: BodyProps["weight"];
   };
   promoCTA: {
-    variant: ButtonProps['variant'];
+    variant: ButtonProps["variant"];
   };
 };
 const deliveryPromoFields: Fields<DeliveryPromoProps> = {
   imageMode: {
-    label: 'Image Mode',
-    type: 'radio',
+    label: "Image Mode",
+    type: "radio",
     options: [
       { label: "Left", value: "left" },
       { label: "Right", value: "right" },
@@ -97,62 +98,81 @@ const deliveryPromoFields: Fields<DeliveryPromoProps> = {
   },
 };
 
-const DeliveryPromo = ({imageMode, promoTitle, promoDescription, promoCTA}: DeliveryPromoProps) => {
-  const deliveryPromo: C_deliveryPromo = useDocument<LocationStream>(document => document.c_deliveryPromo);
+const DeliveryPromo = ({
+  imageMode,
+  promoTitle,
+  promoDescription,
+  promoCTA,
+}: DeliveryPromoProps) => {
+  const deliveryPromo: C_deliveryPromo = useDocument<LocationStream>(
+    (document) => document.c_deliveryPromo
+  );
+
+  const { isMediumDevice } = useScreenSizes();
 
   return (
-      <Section className='components'>
-        <div
-          className={cn(
-            "flex flex-col md:flex-row bg-white overflow-hidden rounded-[30px] md:gap-8",
-            imageMode === "right" && "md:flex-row-reverse",
-          )}
-        >
-          {deliveryPromo.image && <Image
-              image={deliveryPromo.image}
-          />}
-          <div className='flex flex-col justify-center gap-y-4 md:gap-y-8 p-4 md:px-16 md:py-0'>
-            {deliveryPromo.title && <Heading
-                size={promoTitle.size}
-                color={promoTitle.color}
-            >
+    <Section className="components">
+      <div
+        className={cn(
+          "flex flex-col mx-auto max-w-[343px] bg-white rounded-[30px] md:gap-8 md:max-w-[640px] lg:max-w-full lg:flex-row",
+          imageMode === "right" && "lg:flex-row-reverse"
+        )}
+      >
+        {deliveryPromo.image && (
+          <Image
+            className={cn(
+              "rounded-t-lg lg:rounded-t-none",
+              imageMode === "right" && "lg:rounded-r-lg",
+              imageMode === "left" && "lg:rounded-l-lg"
+            )}
+            image={deliveryPromo.image}
+            layout="fixed"
+            width={isMediumDevice ? 640 : 343}
+            height={isMediumDevice ? 474 : 253}
+          />
+        )}
+        <div className="flex flex-col justify-center gap-y-4 md:gap-y-8 p-4 md:px-16 md:py-0">
+          {deliveryPromo.title && (
+            <Heading size={promoTitle.size} color={promoTitle.color}>
               {deliveryPromo.title}
-            </Heading>}
-            {deliveryPromo.description && <Body
-                size={promoDescription.size}
-                weight={promoDescription.weight}
-            >
+            </Heading>
+          )}
+          {deliveryPromo.description && (
+            <Body size={promoDescription.size} weight={promoDescription.weight}>
               {deliveryPromo.description}
-            </Body>}
-            {deliveryPromo.cta && <CTA
-                variant={promoCTA.variant}
-                label={deliveryPromo.cta.name}
-                url={deliveryPromo.cta.link ?? '#'}
-            />}
-          </div>
+            </Body>
+          )}
+          {deliveryPromo.cta && (
+            <CTA
+              variant={promoCTA.variant}
+              label={deliveryPromo.cta.name}
+              url={deliveryPromo.cta.link ?? "#"}
+            />
+          )}
         </div>
-      </Section>
+      </div>
+    </Section>
   );
 };
 
 export const DeliveryPromoComponent: ComponentConfig<DeliveryPromoProps> = {
   fields: deliveryPromoFields,
   defaultProps: {
-    imageMode: 'right',
+    imageMode: "right",
     promoTitle: {
-      size: 'section',
-      color: 'default',
+      size: "section",
+      color: "default",
     },
     promoDescription: {
       size: "base",
       weight: "default",
     },
     promoCTA: {
-      variant: 'default',
+      variant: "default",
     },
   },
-  label: 'Delivery Promo',
-  render: ({imageMode, promoTitle, promoDescription, promoCTA}) => (
+  label: "Delivery Promo",
+  render: ({ imageMode, promoTitle, promoDescription, promoCTA }) => (
     <DeliveryPromo
       imageMode={imageMode}
       promoTitle={promoTitle}
