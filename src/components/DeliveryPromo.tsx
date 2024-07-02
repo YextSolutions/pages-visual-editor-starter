f26f2fd8f21319be475edd729fff0ead745310e4
Brian Baugher <bbaugher@yext.com>
2024-07-02 13:47:37 -0400

    fill

--- src/components/DeliveryPromo.tsx
+++ src/components/DeliveryPromo.tsx
@@ -123,6 +123,7 @@ const DeliveryPromo = ({
           <Image
             image={deliveryPromo.image}
             className={'flex-none'}
+            layout={"fill"}
           />
         )}
         <div className="flex flex-col justify-center gap-y-4 md:gap-y-8 p-4 md:px-16 md:py-0">
