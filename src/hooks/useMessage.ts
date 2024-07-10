a80083e8b87cdaca9680c05da5addef04009cf88
Duval Kilpatrick <mkilpatrick@yext.com>
2024-07-10 13:12:26 -0400

    refactor

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -1,8 +1,17 @@
 // Inspired by https://github.com/rottitime/react-hook-window-message-event
 
-import { RefObject, useState, useRef, useCallback, useEffect } from "react";
+import {
+  RefObject,
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from "react";
 
 export type Payload = Record<string, unknown>;
+export type Status = "success" | "error";
 
 export type PostMessage = {
   payload: Payload;
@@ -14,16 +23,16 @@ type PostMessageInternal = {
 };
 
 export type ReceivePayload = {
-  status: "success" | "error";
+  status: Status;
   payload: Payload;
 };
 
-export type ReceivePayloadInternal = {
+type ReceivePayloadInternal = {
   type: string;
-  status: "success" | "error";
+  status: Status;
 };
 
-export type MessageStatus = "pending" | "error" | "success";
+export type MessageStatus = Status | "pending";
 
 export type EventHandler = (
   callback: (data: ReceivePayload) => unknown,
@@ -48,21 +57,11 @@ export const useSendMessageToIFrame = (
   targetOrigins: string[],
   iframeRef: RefObject<HTMLIFrameElement>
 ) => {
-  const [origin, setOrigin] = useState<string>("");
-  const [source, setSource] = useState<MessageEvent["source"] | null>(null);
   const [status, setStatus] = useState<MessageStatus>("pending");
 
-  const originRef = useRef<string>("");
-  const sourceRef = useRef<MessageEvent["source"]>(null);
-
-  originRef.current = origin;
-  sourceRef.current = source as MessageEvent["source"];
+  useSendMessage(messageName, targetOrigins, statusSetter(setStatus));
 
   const sendToIFrame = (data: PostMessage) => {
-    if (!iframeRef) {
-      throw new Error("IFrame ref not set");
-    }
-
     if (iframeRef.current) {
       setStatus("pending");
       for (const targetOrigin of targetOrigins) {
@@ -74,36 +73,6 @@ export const useSendMessageToIFrame = (
       }
     }
   };
-
-  const onWatchEventHandler = useCallback(
-    ({ origin, source, data }: MessageEvent) => {
-      // Ignore React Dev Tools messages
-      if (data.source === "react-devtools-content-script") {
-        return;
-      }
-
-      if (!targetOrigins.includes(origin)) {
-        throw new Error("Unrecognized origin");
-      }
-
-      const { type, status }: ReceivePayloadInternal = data;
-      if (type === messageName) {
-        setSource(source);
-        setOrigin(origin);
-        if (status === "success") {
-          setStatus("success");
-        } else if (status === "error") {
-          setStatus("error");
-        }
-      }
-    },
-    [messageName, targetOrigins, setSource, setOrigin, setStatus]
-  );
-
-  useEffect(() => {
-    window.addEventListener("message", onWatchEventHandler);
-    return () => window.removeEventListener("message", onWatchEventHandler);
-  }, [messageName, source, origin, onWatchEventHandler]);
 
   return { sendToIFrame, status };
 };
@@ -118,15 +87,9 @@ export const useSendMessageToParent = (
   messageName: string,
   targetOrigins: string[]
 ) => {
-  const [origin, setOrigin] = useState<string>("");
-  const [source, setSource] = useState<MessageEvent["source"] | null>(null);
   const [status, setStatus] = useState<MessageStatus>("pending");
 
-  const originRef = useRef<string>("");
-  const sourceRef = useRef<MessageEvent["source"]>(null);
-
-  originRef.current = origin;
-  sourceRef.current = source as MessageEvent["source"];
+  useSendMessage(messageName, targetOrigins, statusSetter(setStatus));
 
   const sendToParent = (data: PostMessage) => {
     if (!window.parent) {
@@ -137,35 +100,6 @@ export const useSendMessageToParent = (
       postMessage({ ...data, type: messageName }, window.parent, targetOrigin);
     }
   };
-
-  const onWatchEventHandler = useCallback(
-    ({ origin, source, data }: MessageEvent) => {
-      // Ignore React Dev Tools messages
-      if (data.source === "react-devtools-content-script") {
-        return;
-      }
-      if (!targetOrigins.includes(origin)) {
-        throw new Error("Unrecognized origin");
-      }
-
-      const { type, status }: ReceivePayloadInternal = data;
-      if (type === messageName) {
-        setSource(source);
-        setOrigin(origin);
-        if (status === "success") {
-          setStatus("success");
-        } else if (status === "error") {
-          setStatus("error");
-        }
-      }
-    },
-    [messageName, targetOrigins, setSource, setOrigin, setStatus]
-  );
-
-  useEffect(() => {
-    window.addEventListener("message", onWatchEventHandler);
-    return () => window.removeEventListener("message", onWatchEventHandler);
-  }, [messageName, source, origin, onWatchEventHandler]);
 
   return { sendToParent, status };
 };
@@ -182,11 +116,43 @@ export const useReceiveMessage = (
   targetOrigins: string[],
   eventHandler: EventHandler
 ) => {
-  const [origin, setOrigin] = useState<string>("");
-  const [source, setSource] = useState<MessageEvent["source"] | null>(null);
+  const handleMessageAndSendResponse = (
+    data: any,
+    source: MessageEvent["source"]
+  ) => {
+    const { type, payload }: { type: string; payload: Payload } = data;
 
-  const originRef = useRef<string>("");
-  const sourceRef = useRef<MessageEvent["source"]>(null);
+    if (type === messageName) {
+      if (typeof eventHandler === "function") {
+        eventHandler(
+          (data) => postMessage({ ...data, type: messageName }, source, origin),
+          payload
+        );
+      }
+    }
+  };
+
+  useSendMessage(messageName, targetOrigins, handleMessageAndSendResponse);
+};
+
+/**
+ * An internal hook which listens for a specific message type, and when it receives it,
+ * it calls the event handler with the message payload and a function to send a message back
+ * to the sender.
+ * @param messageName - The message name to listen on
+ * @param targetOrigins - The origin urls the message can be posted to and received from
+ * @param callback - Callback function to handle the response data and message source
+ */
+const useSendMessage = (
+  messageName: string,
+  targetOrigins: string[],
+  callback: (data: any, source: MessageEvent["source"]) => void
+) => {
+  const [origin, setOrigin] = useState<string>();
+  const [source, setSource] = useState<MessageEvent["source"]>();
+
+  const originRef = useRef<string>();
+  const sourceRef = useRef<MessageEvent["source"]>();
 
   originRef.current = origin;
   sourceRef.current = source as MessageEvent["source"];
@@ -201,24 +167,31 @@ export const useReceiveMessage = (
         throw new Error("Unrecognized origin");
       }
 
-      const { type, payload }: { type: string; payload: Payload } = data;
+      const { type }: ReceivePayloadInternal = data;
       if (type === messageName) {
         setSource(source);
         setOrigin(origin);
-        if (typeof eventHandler === "function") {
-          eventHandler(
-            (data) =>
-              postMessage({ ...data, type: messageName }, source, origin),
-            payload
-          );
-        }
+
+        callback(data, source);
       }
     },
-    [messageName, targetOrigins, eventHandler, setSource, setOrigin]
+    [messageName, targetOrigins, setSource, setOrigin, callback]
   );
 
   useEffect(() => {
     window.addEventListener("message", onWatchEventHandler);
     return () => window.removeEventListener("message", onWatchEventHandler);
   }, [messageName, source, origin, onWatchEventHandler]);
+};
+
+const statusSetter = (setStatus: Dispatch<SetStateAction<MessageStatus>>) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return (data: any, source: MessageEvent["source"]) => {
+    const { status }: { status: Status } = data;
+    if (status === "success") {
+      setStatus("success");
+    } else if (status === "error") {
+      setStatus("error");
+    }
+  };
 };
