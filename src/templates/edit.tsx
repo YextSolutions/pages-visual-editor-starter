7a1a99de725a7e3a8b5a7a5f79171527adf55605
Brian Stephan <bstephan@yext.com>
2024-07-29 15:29:55 -0400

    default empty object

--- src/templates/edit.tsx
+++ src/templates/edit.tsx
@@ -233,7 +233,7 @@ const Edit: () => JSX.Element = () => {
       );
       const localHistoryArray = localHistory ? JSON.parse(localHistory) : [];
       const historyToSend = JSON.stringify(localHistoryArray.length > 0 ? 
-        localHistoryArray[localHistoryArray.length-1].data : "");
+        localHistoryArray[localHistoryArray.length-1].data : {});
       sendHistory({ payload: { history: historyToSend } });
     }
   }, [templateMetadata]);
