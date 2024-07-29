508f32b5c541cea8d8966d33ff528537171489e1
apav-dev <apavlick@yext.com>
2024-07-29 16:39:27 -0400

    update stream filter to use entityIds instead of savedFilterIds

--- src/templates/financialProfessional.tsx
+++ src/templates/financialProfessional.tsx
@@ -18,7 +18,7 @@ export const config: TemplateConfig = {
   stream: {
     $id: "financialProfessional-stream",
     filter: {
-      savedFilterIds: [YEXT_PUBLIC_FIN_PRO_ID],
+      entityIds: [YEXT_PUBLIC_FIN_PRO_ID],
     },
     fields: [
       "id",
