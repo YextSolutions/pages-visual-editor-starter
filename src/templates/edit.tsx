3670d5b9683ed42371c7934a3cf81755f2ae693c
Colton Demetriou <cdemetriou@yext.com>
2024-08-05 15:25:45 -0400

    refactor 2
    Change-Id: Ie1891d0c225b822f28e7173798caf5d4cb1c780f

--- src/templates/edit.tsx
+++ src/templates/edit.tsx
@@ -374,14 +374,17 @@ const Edit: () => JSX.Element = () => {
         //   <LoadingScreen progress={progress} />
         // ) : 
         // !hasParent && redirectTo404()
-        useEffect(() => {
-          if (typeof window !== "undefined" && !hasParent) {
-            window.location.href = "/404.html";
-          }
-          if (typeof window !== "undefined" && hasParent) {
-            <LoadingScreen progress={progress} />
-          }
-        }, [hasParent])
+        <>
+        {
+          useEffect(() => {
+            if (typeof window !== "undefined" && !hasParent) {
+              window.location.href = "/404.html";
+            }
+          }, [hasParent])
+        }
+        {typeof window !== "undefined" && hasParent && <LoadingScreen progress={progress} />}
+        </>
+      
       )
       }
       <Toaster closeButton richColors />
