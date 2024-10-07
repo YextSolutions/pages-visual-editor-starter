5ba2b5c1883c0ec01ed3e39cdaae0a08d1bdef38
Ben Life <blife@yext.com>
2024-10-07 15:32:10 -0400

    use auto css variables

--- src/templates/edit.tsx
+++ src/templates/edit.tsx
@@ -1,12 +1,21 @@
 import {
+  applyTheme,
   Editor,
   EntityFieldsProvider,
   usePlatformBridgeDocument,
   usePlatformBridgeEntityFields,
 } from "@yext/visual-editor";
-import {componentRegistry} from "../ve.config";
-import {GetPath, TemplateProps, TemplateConfig} from "@yext/pages";
-import {DocumentProvider} from "@yext/pages/util";
+import { componentRegistry } from "../ve.config";
+import {
+  GetPath,
+  TemplateProps,
+  TemplateConfig,
+  HeadConfig,
+  TemplateRenderProps,
+  GetHeadConfig,
+} from "@yext/pages";
+import { DocumentProvider } from "@yext/pages/util";
+import { themeConfig } from "../../theme.config";
 
 // Editor is avaliable at /edit
 export const getPath: GetPath<TemplateProps> = () => {
@@ -17,6 +26,14 @@ export const config: TemplateConfig = {
   name: "edit",
 };
 
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  document,
+}): HeadConfig => {
+  return {
+    other: applyTheme(document, themeConfig),
+  };
+};
+
 // Render the editor
 const Edit: () => JSX.Element = () => {
   const entityDocument = usePlatformBridgeDocument();
@@ -25,7 +42,11 @@ const Edit: () => JSX.Element = () => {
   return (
     <DocumentProvider value={entityDocument}>
       <EntityFieldsProvider entityFields={entityFields}>
-        <Editor document={entityDocument} componentRegistry={componentRegistry} />
+        <Editor
+          document={entityDocument}
+          componentRegistry={componentRegistry}
+          themeConfig={themeConfig}
+        />
       </EntityFieldsProvider>
     </DocumentProvider>
   );
---
+++ theme.config.ts
@@ -0,0 +1,153 @@
+import { ThemeConfig } from "@yext/visual-editor";
+
+export const themeConfig: ThemeConfig = {
+  text: {
+    label: "Text",
+    styles: {
+      size: { label: "Size", type: "number", plugin: "text", default: 14 },
+    },
+  },
+  colors: {
+    label: "Colors",
+    styles: {
+      text: {
+        label: "Text",
+        plugin: "colors",
+        type: "color",
+        default: "black",
+      },
+      border: {
+        label: "Border",
+        plugin: "colors",
+        type: "color",
+        default: "hsl(214 100% 39%)",
+      },
+      input: {
+        label: "Input",
+        plugin: "colors",
+        type: "color",
+        default: "hsl(214.3 31.8% 91.4%)",
+      },
+      ring: {
+        label: "Ring",
+        plugin: "colors",
+        type: "color",
+        default: "hsl(215 20.2% 65.1%)",
+      },
+      background: {
+        label: "Background",
+        plugin: "colors",
+        type: "color",
+        default: "hsl(0 0% 100%)",
+      },
+      foreground: {
+        label: "Foreground",
+        plugin: "colors",
+        type: "color",
+        default: "hsl(0 2% 11%)",
+      },
+      primary: {
+        label: "Primary",
+        styles: {
+          DEFAULT: {
+            label: "Default",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 68% 51%)",
+          },
+          foreground: {
+            label: "Foreground",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+        },
+      },
+      secondary: {
+        label: "Secondary",
+        styles: {
+          DEFAULT: {
+            label: "Default",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(11 100% 26%)",
+          },
+          foreground: {
+            label: "Foreground",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+        },
+      },
+      destructive: {
+        label: "Destructive",
+        styles: {
+          DEFAULT: {
+            label: "Default",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 100% 50%)",
+          },
+          foreground: {
+            label: "Foreground",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(210 40% 98%)",
+          },
+        },
+      },
+      muted: {
+        label: "Muted",
+        styles: {
+          DEFAULT: {
+            label: "Default",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(210 40% 96.1%)",
+          },
+          foreground: {
+            label: "Foreground",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(215.4 16.3% 46.9%)",
+          },
+        },
+      },
+      accent: {
+        label: "Accent",
+        styles: {
+          DEFAULT: {
+            label: "Default",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(166 55% 67%)",
+          },
+          foreground: {
+            label: "Foreground",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 0%)",
+          },
+        },
+      },
+      popover: {
+        label: "Secondary",
+        styles: {
+          DEFAULT: {
+            label: "Default",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(225 7% 12%)",
+          },
+          foreground: {
+            label: "Foreground",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+        },
+      },
+    },
+  },
+};
