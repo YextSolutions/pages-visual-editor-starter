dd892576b72d2913f4fe44c54b9c677684cd0f3a
Jacob Wartofsky <jwartofsky@yext.com>
2024-10-24 18:26:32 -0400

    Update themes

--- theme.config.ts
+++ theme.config.ts
@@ -15,6 +15,52 @@ export const themeConfig: ThemeConfig = {
   colors: {
     label: "Colors",
     styles: {
+      primary: {
+        label: "Buttons",
+        styles: {
+          DEFAULT: {
+            label: "Button Background",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 68% 51%)",
+          },
+          foreground: {
+            label: "Button Text",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+        },
+      },
+      header: {
+        label: "Color Palette",
+        styles: {
+          primary: {
+            label: "Primary",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+          secondary: {
+            label: "Secondary",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+          accent: {
+            label: "Accent",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+          text: {
+            label: "Text",
+            plugin: "colors",
+            type: "color",
+            default: "hsl(0 0% 100%)",
+          },
+        },
+      },
       foreground: {
         label: "Footer",
         plugin: "colors",
@@ -24,23 +70,6 @@ export const themeConfig: ThemeConfig = {
           { value: "#808080", label: "Grey" },
         ],
         default: "#808080",
-      },
-      primary: {
-        label: "Primary",
-        styles: {
-          DEFAULT: {
-            label: "Main",
-            plugin: "colors",
-            type: "color",
-            default: "hsl(0 68% 51%)",
-          },
-          foreground: {
-            label: "Accent",
-            plugin: "colors",
-            type: "color",
-            default: "hsl(0 0% 100%)",
-          },
-        },
       },
     },
   },
