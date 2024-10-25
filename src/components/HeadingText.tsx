b0342a5c1c4cdaa441f6c4a8a92fad86eb733278
Ben Life <blife@yext.com>
2024-10-25 12:58:06 -0400

    demo clean up

--- src/components/HeadingText.tsx
+++ src/components/HeadingText.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { ComponentConfig, Fields } from "@measured/puck";
-import { Heading, HeadingProps, headingVariants } from "./atoms/heading1.js";
+import { Heading, HeadingProps, headingVariants } from "./atoms/heading.js";
 import {
   EntityField,
   YextEntityField,
   YextEntityFieldSelector,
   resolveYextEntityField,
-  useDocument
+  useDocument,
 } from "@yext/visual-editor";
 
 export interface HeadingTextProps extends HeadingProps {
@@ -14,17 +14,17 @@ export interface HeadingTextProps extends HeadingProps {
 }
 
 const HeadingText = React.forwardRef<HTMLHeadingElement, HeadingTextProps>(
-    ({ text, ...headingProps }, ref) => {
-      const document = useDocument();
+  ({ text, ...headingProps }, ref) => {
+    const document = useDocument();
 
-      return (
-          <EntityField fieldId={text.field}>
-            <Heading ref={ref} {...headingProps}>
-              {resolveYextEntityField(document, text)}
-            </Heading>
-          </EntityField>
-      );
-    }
+    return (
+      <EntityField fieldId={text.field}>
+        <Heading ref={ref} {...headingProps}>
+          {resolveYextEntityField(document, text)}
+        </Heading>
+      </EntityField>
+    );
+  }
 );
 
 HeadingText.displayName = "HeadingText";
@@ -58,11 +58,6 @@ const headingTextFields: Fields<HeadingTextProps> = {
       { label: "Black", value: "black" },
     ],
   },
-  // size: {
-  //   label: "Text Size",
-  //   type: "number",
-  //   min: 1,
-  // },
   color: {
     type: "select",
     options: [
@@ -96,8 +91,8 @@ export const HeadingTextComponent: ComponentConfig<HeadingTextProps> = {
     },
     content: "Heading",
     level: 2,
-    weight: "bold",
-    color: "primary",
+    weight: "default",
+    color: "default",
     transform: "none",
   },
   render: (props) => <HeadingText {...props} />,
--- src/components/atoms/heading.tsx
+++ src/components/atoms/heading.tsx
@@ -1,25 +1,47 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "../../utils/cn";
 
 // Define the variants for the heading component
-const headingVariants = cva("font-bold", {
+const headingVariants = cva("py-2", {
   variants: {
-    size: {
-      page: "text-5xl",
-      section: "text-[34px]",
-      subheading: "text-2xl",
+    level: {
+      1: "font-heading1 text-heading1-fontSize ve-text-heading1-color",
+      2: "font-heading2 text-heading2-fontSize ve-text-heading2-color",
+      3: "font-heading3 text-heading3-fontSize text-heading3-color",
+      4: "font-heading4 text-heading4-fontSize text-heading4-color",
+      5: "font-heading5 text-heading5-fontSize text-heading5-color",
+      6: "font-heading6 text-heading6-fontSize text-heading6-color",
+    },
+    weight: {
+      default: "",
+      thin: "font-thin",
+      extralight: "font-extralight",
+      light: "font-light",
+      normal: "font-normal",
+      medium: "font-medium",
+      semibold: "font-semibold",
+      bold: "font-bold",
+      extrabold: "font-extrabold",
+      black: "font-black",
     },
     color: {
-      default: "text-default",
-      primary: "text-primary",
-      secondary: "text-secondary",
-      accent: "text-accent",
+      default: "",
+      primary: "text-header-primary",
+      secondary: "text-header-secondary",
+      accent: "text-header-accent",
+      text: "text-header-text",
+    },
+    transform: {
+      none: "",
+      uppercase: "uppercase",
+      lowercase: "lowercase",
+      capitalize: "capitalize",
     },
   },
   defaultVariants: {
-    size: "section",
-    color: "default",
+    color: "primary",
+    weight: "default",
+    transform: "none",
   },
 });
 
@@ -34,7 +56,7 @@ export interface HeadingProps
 }
 
 const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
-  ({ className, level = 1, size, color, ...props }, ref) => {
+  ({ className, level = 1, color, weight, transform, ...props }, ref) => {
     const Tag = `h${level}` as keyof Pick<
       JSX.IntrinsicElements,
       "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
@@ -42,14 +64,18 @@ const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
 
     return (
       <Tag
-        className={cn(headingVariants({ size, color, className }))}
+        id="tag"
+        className={
+          "components " +
+          headingVariants({ color, className, weight, transform, level })
+        }
         ref={ref}
         {...props}
       >
         {props.children}
       </Tag>
     );
-  },
+  }
 );
 Heading.displayName = "Heading";
 
