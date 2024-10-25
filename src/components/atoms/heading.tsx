babcb064ff2a28cabda533397522030583998da8
Ben Life <blife@yext.com>
2024-10-25 13:02:53 -0400

    move base class

--- src/components/atoms/heading.tsx
+++ src/components/atoms/heading.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 // Define the variants for the heading component
-const headingVariants = cva("py-2", {
+const headingVariants = cva("components py-2", {
   variants: {
     level: {
       1: "font-heading1 text-heading1-fontSize text-heading1-color",
@@ -65,10 +65,13 @@ const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
     return (
       <Tag
         id="tag"
-        className={
-          "components " +
-          headingVariants({ color, className, weight, transform, level })
-        }
+        className={headingVariants({
+          color,
+          className,
+          weight,
+          transform,
+          level,
+        })}
         ref={ref}
         {...props}
       >
