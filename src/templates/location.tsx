aaf0f974bbd046aab9d8680bb2361942905aa599
Jacob Wartofsky <jwartofsky@yext.com>
2024-12-18 16:53:42 -0500

    Revert "Update location template"
    This reverts commit a66ca6fa6281af8e4b2b2e910d10d7f801e3e7d4.

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -11,11 +11,11 @@ import { Config, Render } from "@measured/puck";
 import { locationConfig } from "../ve.config";
 import { applyTheme, VisualEditorProvider } from "@yext/visual-editor";
 import { themeConfig } from "../../theme.config";
-import { buildSchema } from "../utils/buildSchema";
+// import { buildSchema } from "../utils/buildSchema";
 
 export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
-                                                                    document,
-                                                                  }): HeadConfig => {
+  document,
+}): HeadConfig => {
   return {
     title: document.name,
     charset: "UTF-8",
@@ -29,34 +29,29 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
         },
       },
     ],
-    other: [applyTheme(document, themeConfig), buildSchema(document)].join(
-        "\n"
-    ),
+    other: applyTheme(document, themeConfig),
   };
 };
 
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
-  if (!document?.__?.layout) {
-    // temporary: guard for generated repo-based static page
+  if (!document?.address?.region) {
     return `static-${Math.floor(Math.random() * (10000 - 1))}`;
   }
   return document.slug
-      ? document.slug
-      : `${document.locale}/${document.address.region}/${document.address.city}/${
-          document.address.line1
+    ? document.slug
+    : `${document.locale}/${document.address.region}/${document.address.city}/${
+        document.address.line1
       }-${document.id.toString()}`;
 };
 
 const Location: Template<TemplateRenderProps> = ({ document }) => {
-  // temporary: guard for generated repo-based static page
   if (!document?.__?.layout) {
-    return <>Location STATIC page</>;
+    return <p>Location static page.</p>;
   }
-
   return (
-      <VisualEditorProvider document={document}>
-        <Render config={locationConfig as Config} data={document.__.layout} />
-      </VisualEditorProvider>
+    <VisualEditorProvider document={document}>
+      <Render config={locationConfig as Config} data={document.__.layout} />
+    </VisualEditorProvider>
   );
 };
 
