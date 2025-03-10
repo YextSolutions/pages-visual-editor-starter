4eadd2a0d4d421eefbc57f7a383e3a77f0bbd966
Jacob Wartofsky <jwartofsky@yext.com>
2025-03-10 14:05:41 -0400

    fix module

---
+++ src/modules/analyticsTest/analyticsTest.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Module, ModuleConfig, ModuleProps } from "@yext/pages/*";
+import { AnalyticsProvider, useAnalytics } from "@yext/pages-components";
+import "./index.css";
+
+const templateData: ModuleProps = {
+  document: {
+    businessId: "REPLACE_ME",
+    siteId: "REPLACE_ME",
+    __: {
+      name: "analyticsTest",
+      staticPage: true
+    }
+  }, 
+  __meta: {
+    mode: "production"
+  }
+}
+
+export const config: ModuleConfig = {
+  name: "analyticsTest"
+}
+
+const WidgetInterior = () => {
+  useAnalytics();
+  return <p>widget potato widget</p>;
+}
+
+const AnalyticsTest: Module = () => {
+  return(
+    <AnalyticsProvider 
+      apiKey={YEXT_PUBLIC_SEARCH_EXPERIENCE_API_KEY}
+      currency="USD"
+      templateData={templateData}
+      productionDomains={["jwartofskyanalyticstest.com"]}
+    >
+      <WidgetInterior></WidgetInterior>
+    </AnalyticsProvider>
+  )
+}
+
+export default AnalyticsTest;
