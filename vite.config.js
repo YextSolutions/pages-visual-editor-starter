2e0b3c4c08e64e8fb3d6843738a037550e9ff133
Alexis Sanehisa <asanehisa@yext.com>
2025-04-24 16:50:31 -0400

    update to 35
    Change-Id: I27751233ed6abffeddbdbfb711dc46b3ddd5c2be

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
+import { yextVisualEditorPlugin } from "@yext/visual-editor/plugin"
 
 export default defineConfig({
-  plugins: [react(), yextSSG()],
+  plugins: [react(), yextVisualEditorPlugin(), yextSSG()],
 });
