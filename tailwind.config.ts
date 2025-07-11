3293c8b95f1c105dd69667a0b75e0d258fe9c874
Maxfield Kouzel <mkouzel@yext.com>
2025-07-11 12:08:36 -0400

    fix: include search-ui-react tailwind classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,13 @@ import {
   themeResolver,
   defaultThemeTailwindExtensions,
 } from "@yext/visual-editor";
+import {ComponentsContentPath as SearchUiComponentsContentPath} from "@yext/search-ui-react";
 
 export default {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
     "./node_modules/@yext/visual-editor/dist/**/*.js",
+    SearchUiComponentsContentPath
   ],
   theme: {
     extend: themeResolver(defaultThemeTailwindExtensions, themeConfig),
