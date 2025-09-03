d9326d5d410f5720d2d0653739fddb9effba9ff5
Ben Life <blife@yext.com>
2025-09-03 11:25:34 -0400

    move theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
 import type { Config } from "tailwindcss";
-import { themeConfig } from "./theme.config";
 import {
   themeResolver,
   defaultThemeTailwindExtensions,
+  defaultThemeConfig,
 } from "@yext/visual-editor";
-import {ComponentsContentPath as SearchUiComponentsContentPath} from "@yext/search-ui-react";
+import { ComponentsContentPath as SearchUiComponentsContentPath } from "@yext/search-ui-react";
 
 export default {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
     "./node_modules/@yext/visual-editor/dist/**/*.js",
-    SearchUiComponentsContentPath
+    SearchUiComponentsContentPath,
   ],
   theme: {
-    extend: themeResolver(defaultThemeTailwindExtensions, themeConfig),
+    extend: themeResolver(defaultThemeTailwindExtensions, defaultThemeConfig),
   },
   plugins: [],
 } satisfies Config;
