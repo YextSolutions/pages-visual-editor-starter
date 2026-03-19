9af2e6cb01df3aad1cf29e4439ff8238e0c0df5a
Jacob Wartofsky <jwartofsky@yext.com>
2026-03-19 11:19:13 -0400

    remove templates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
 import { webcrypto } from "node:crypto";
-import { execFile } from "node:child_process";
-import { promisify } from "node:util";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
@@ -10,33 +8,6 @@ if (!globalThis.crypto) {
   globalThis.crypto = webcrypto;
 }
 
-const execFileAsync = promisify(execFile);
-
-const applyGeneratedTemplateConfigPlugin = () => {
-  let hasRun = false;
-
-  return {
-    name: "apply-generated-template-config",
-    async buildStart() {
-      if (hasRun) {
-        return;
-      }
-      hasRun = true;
-
-      await execFileAsync(process.execPath, [
-        "--import",
-        "tsx",
-        "scripts/generateTemplateConfig.ts",
-      ]);
-    },
-  };
-};
-
 export default defineConfig({
-  plugins: [
-    react(),
-    yextVisualEditorPlugin(),
-    applyGeneratedTemplateConfigPlugin(),
-    yextSSG(),
-  ],
+  plugins: [react(), yextVisualEditorPlugin(), yextSSG()],
 });
