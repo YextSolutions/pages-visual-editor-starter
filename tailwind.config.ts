f3d5409d131a5f88ee6491142a2ed977f1d6e739
Ben Life <77246839+benlife5@users.noreply.github.com>
2026-06-30 13:52:09 -0400

    chore: standardize package.json (#340)
    Added:
    - A few more radix primitives so they are available for use
    - tsx dev dependency
    
    Moved:
    - pages, visual-editor, and pages-components should be regular
    dependencies
    - types/node and tailwindcss-animate should be devDependencies
    - Imported tailwindcss-animate into the config so it actually works
    
    Removed:
    - next-themes: doesn't seem very relevant
    - classnames: clsx can handle all of this
    - tailwindcss/typography: doesn't seem relevant unless specifically
    asked for

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 import {
   themeResolver,
   defaultThemeTailwindExtensions,
@@ -16,5 +17,5 @@ export default {
   theme: {
     extend: themeResolver(defaultThemeTailwindExtensions, defaultThemeConfig),
   },
-  plugins: [],
+  plugins: [animate],
 } satisfies Config;
